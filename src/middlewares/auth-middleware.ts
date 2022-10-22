2e40caf83f0032ba4822d3e0e9dcbe61ed33f268
Степан <kokorin121@yandex.ru>
2022-10-23 02:15:26 +0500

    .

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -3,11 +3,14 @@ import { NextFunction, Request, Response } from "express";
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
 
   const authorization = req.header('authorization')
-  const validAuth = btoa('admin' + ":" + 'qwerty')
+  const validAuth = "Basic" + " " + btoa('admin' + ":" + 'qwerty')
+
+  console.log(validAuth);
+  console.log(authorization);
 
 
 
-  if (authorization && ('Basic' + ' ' + authorization === validAuth)) {
+  if (authorization && (authorization === validAuth)) {
    next()
   } else {
     res.sendStatus(401)
